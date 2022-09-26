486a92482537e7ce3c7b78389a2b19ae1647c284
Timofey-Makhankov <timofey.makahnkov@noseryoung.com>
2022-09-26 10:44:19 +0200

    finished ecxercise a & b

--- src/App.java
+++ src/App.java
@@ -6,22 +6,31 @@ public class App {
     public static int inputNumber3;
 
     public static void main(String[] args) throws Exception {
-        inputNumber1 = Input.inputInt("Input first Digit:");
-        inputNumber2 = Input.inputInt("Input second Digit:");
-        inputNumber3 = Input.inputInt("Input third Digit:");
 
-        for (int i = 0; i < 4; i++) {
+        boolean isRunning = true;
+
+        while (isRunning) {
+            inputNumber1 = Input.inputInt("Input first Digit:");
+            inputNumber2 = Input.inputInt("Input second Digit:");
+            inputNumber3 = Input.inputInt("Input third Digit:");
+
+            if (inputNumber1 == inputNumber2 && inputNumber2 == inputNumber3) {
+                break;
+            }
+
             if (inputNumber1 > inputNumber2) {
                 Tausche1();
-            }else if (inputNumber2 > inputNumber3) {
-                Tausche2();
-            } else {
-                continue;
             }
+            if (inputNumber2 > inputNumber3) {
+                Tausche2();
+            }
+            if (inputNumber1 > inputNumber2) {
+                Tausche1();
+            }
+
+            System.out.println(
+                    inputNumber1 + " ist kleiner als " + inputNumber2 + " ist kleiner als " + inputNumber3 + " !");
         }
-
-        System.out.println(inputNumber1 + " ist kleiner als " + inputNumber2 + " ist kleiner als " + inputNumber3 + " 10!");
-
     }
 
     static void Tausche1() {
