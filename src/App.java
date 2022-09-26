9ff280485aaf3d2e140a4db2d8a24ff26e0b2e74
Timofey-Makhankov <timofey.makahnkov@noseryoung.com>
2022-09-26 10:16:35 +0200

    finished Code

--- src/App.java
+++ src/App.java
@@ -1,5 +1,38 @@
+import lib.Input;
+
 public class App {
+    public static int inputNumber1;
+    public static int inputNumber2;
+    public static int inputNumber3;
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        inputNumber1 = Input.inputInt("Input first Digit:");
+        inputNumber2 = Input.inputInt("Input second Digit:");
+        inputNumber3 = Input.inputInt("Input third Digit:");
+
+        for (int i = 0; i < 4; i++) {
+            if (inputNumber1 > inputNumber2) {
+                Tausche1();
+            }else if (inputNumber2 > inputNumber3) {
+                Tausche2();
+            } else {
+                continue;
+            }
+        }
+
+        System.out.println(inputNumber1 + " ist kleiner als " + inputNumber2 + " ist kleiner als " + inputNumber3 + " 10!");
+
+    }
+
+    static void Tausche1() {
+        int temp = inputNumber1;
+        inputNumber1 = inputNumber2;
+        inputNumber2 = temp;
+    }
+
+    static void Tausche2() {
+        int temp = inputNumber3;
+        inputNumber3 = inputNumber2;
+        inputNumber2 = temp;
     }
 }
